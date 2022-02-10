b07ac688a16e5e133285b8b6a9eb7c4ae784418e
Chris Siannas <xrsia@otenet.gr>
2022-02-10 14:02:41 +0200

    Final

--- embed-structures.go
+++ embed-structures.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type coords struct {
 	x, y int
 }
@@ -10,5 +12,15 @@ type circle struct {
 }
 
 func main() {
+	var ring circle
+	ring.radius = 15
+	ring.coords.x = ring.radius
+	ring.coords.y = ring.radius
 
+	fmt.Printf("Diameter :%v\n", ring.getDiameter())
+	fmt.Printf("Point X:%v Y:%v\n", ring.coords.x, ring.coords.y)
+}
+
+func (c circle) getDiameter() int {
+	return 2 * c.radius
 }
